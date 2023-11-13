3ad980583a1e1b1034286bdd30c3b98952810f73
comex <costasmexis@gmail.com>
2023-11-13 13:49:11 +0200

    Update

--- gans.py
+++ gans.py
@@ -17,19 +17,8 @@ from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 from sklearn.model_selection import KFold
 from xgboost import XGBRegressor
 from tqdm import tqdm
+from conf import *
 from utils import *
-
-PATH = 'data/limonene_data.csv'
-MAX_ITER = 100000
-RESPONSE_VARS = ['Limonene']
-INPUT_VARS = ['ATOB_ECOLI','ERG8_YEAST','IDI_ECOLI',
-                   'KIME_YEAST','MVD1_YEAST','Q40322_MENSP',
-                   'Q8LKJ3_ABIGR','Q9FD86_STAAU','Q9FD87_STAAU']
-DBTL_A = ['2X-Mh', 'B-Lm', '2X-Ll', 'A-Mm', 'B-Ll', 'A-Mh', '2X-Lm',
-       'A-Hl', '2X-Hh', 'B-Ml', 'B-Mm', '2X-Lh', 'B-Mh', '2X-Hl', 'B-Hl',
-       '2X-Ml', 'B-Hm', 'B-Lh', 'B-Hh', 'A-Ll', 'A-Hm', '2X-Mm', 'A-Hh',
-       'A-Ml', 'A-Lm',  'A-Lh', '2X-Hm']
-DBTL_B = ['BL-Mm', 'BL-Mh', 'BL-Ml']
 
 # %%
 ''' Read original data and transform it for analysis'''
@@ -56,19 +45,30 @@ y_val = val[RESPONSE_VARS]
 X_test = data_B[INPUT_VARS]
 y_test = data_B[RESPONSE_VARS]
 
+# %% 
+''' Train ML model on train set '''
+params_knn = {'n_neighbors': [2, 4, 6, 8], 'weights': ['uniform', 'distance']}
+grid = GridSearchCV(KNeighborsRegressor(n_jobs=-1), params_knn, scoring='neg_mean_absolute_error', cv=5, n_jobs=-1)
+grid.fit(X_train, y_train)
+model = grid.best_estimator_
+model.fit(X_train, y_train)
+y_pred = model.predict(X_val)
+plot_pred_vs_actual(y_val, y_pred, 'Accuracy on Validation')
+
 # %%
 from sdv.single_table import CTGANSynthesizer, GaussianCopulaSynthesizer, CopulaGANSynthesizer, TVAESynthesizer
 from sdv.metadata import SingleTableMetadata
 
 metadata = SingleTableMetadata()
-metadata.detect_from_dataframe(data=train)
+metadata.detect_from_dataframe(data=X_train)
 
-ctgan = GaussianCopulaSynthesizer(metadata)
-ctgan.fit(train)
+ctgan = CTGANSynthesizer(metadata)
+ctgan.fit(X_train)
 
-gen = ctgan.sample(100)
+gen = ctgan.sample(500)
 X_gen = gen[INPUT_VARS]
-y_gen = gen[RESPONSE_VARS]
+y_gen = model.predict(X_gen)
+y_gen = pd.DataFrame(y_gen, columns=y_train.columns)
 
 # %%
 params_knn = {'n_neighbors': [2, 4, 6, 8], 'weights': ['uniform', 'distance']}
@@ -90,10 +90,9 @@ _ = model_validation(grid, X_train, y_train, X_val, y_val, 'Real Data')
 # SYNTHETIC data
 _ = model_validation(grid, X_gen, y_gen, X_val, y_val, 'Synthetic Data')
 
-# Concat X_train and X_gen & y_train and y_gen
+# REAL + SYNTHETIC data
 X_train_gen = pd.concat([X_train, X_gen])
 y_train_gen = pd.concat([y_train, y_gen])
-# REAL + SYNTHETIC data
 _ = model_validation(grid, X_train_gen, y_train_gen, X_val, y_val, 'Real and Synthetic Data')
 
 # %%
@@ -104,16 +103,15 @@ _ = model_validation(grid, X_train, y_train, X_test, y_test, 'Real Data')
 # SYNTHETIC data
 _ = model_validation(grid, X_gen, y_gen, X_test, y_test, 'Synthetic Data')
 
-# Concat X_train and X_gen & y_train and y_gen
+# REAL + SYNTHETIC data
 X_train_gen = pd.concat([X_train, X_gen])
 y_train_gen = pd.concat([y_train, y_gen])
-# REAL + SYNTHETIC data
 _ = model_validation(grid, X_train_gen, y_train_gen, X_test, y_test, 'Real and Synthetic Data')
 
 # %%
 from table_evaluator import TableEvaluator
 
-table_evaluator = TableEvaluator(train, gen)
+table_evaluator = TableEvaluator(X_train, X_gen)
 table_evaluator.visual_evaluation()
 plot_corr_heatmap(gen)
 plot_corr_heatmap(train)
